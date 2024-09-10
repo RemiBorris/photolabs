dce7d3c5d90ce54f8ab06dd3251e690df1d59942
remiborris <remiborris@gmail.com>
2024-09-10 11:38:13 -0300

    add SASS styling per activity

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,11 +7,13 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
   return(
-    <div key={props.id}>
-      <img src={props.imageSource}></img><br />
-      <img src={props.profile}></img>
-      <h2>{props.username}</h2>
-      <h2>{props.city}{props.country}</h2>
+    <div className="photo-list__item" key={props.photo.id}>
+      <img className="photo-list__image" src={props.photo.imageSource}></img><br />
+      <img className="photo-list__user-profile"src={props.photo.profile}></img>
+      <div className="photo-list__user-details">
+        <h2 className="photo-list__user-info">{props.photo.username}</h2>
+        <h2 className="photo-list__location">{props.photo.location.city}, {props.photo.location.country}</h2>
+      </div>
     </div>
   );
 };
