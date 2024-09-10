8222a2aefd3916ed1f6e6f41d8af62c2a49c29fa
remiborris <remiborris@gmail.com>
2024-09-10 09:54:20 -0300

    created the initial photolabs activity to show 1 photo item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+  return(
+    <div key={props.id}>
+      <img src={props.imageSource}></img><br />
+      <img src={props.profile}></img>
+      <h2>{props.username}</h2>
+      <h2>{props.city}{props.country}</h2>
+    </div>
+  );
 };
 
 export default PhotoListItem;
