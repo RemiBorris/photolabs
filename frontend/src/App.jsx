9e70f4c30dda90a3d65e63e5c10af23e7a0b7542
remiborris <remiborris@gmail.com>
2024-09-12 21:08:32 -0300

    create HomeRoute page to hold navBar and PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoFavButton from 'components/PhotoFavButton';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
-
-// const photos = new Array(3).fill(sampleDataForPhotoListItem).map((data, index) => <PhotoListItem photo={data} key={index}/>)
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      {/* {photos} */}
-      {/* <PhotoList /> */}
+      <HomeRoute/>
     </div>
   );
 };
